d97869ea29e12b3237914015ebc9679e8e6d50dd
abdullah <khanmohaiymin@gmail.com>
2024-08-28 23:19:07 +0500

    feat: :art: minin issues resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-
+import Navbar from "/src/components/Navbar";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+  <>
+  <Navbar />
+  </>
   );
 };
 
