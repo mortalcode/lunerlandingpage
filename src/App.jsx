d58b3688bd1a1a8a95f8100ddddf062003afa6ff
abdullah <khanmohaiymin@gmail.com>
2024-08-28 23:54:32 +0500

    docs: :zap: hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "/src/components/Navbar";
 const App = () => {
   return (
   <>
   <Navbar />
+  <HeroSection />
   </>
   );
 };
